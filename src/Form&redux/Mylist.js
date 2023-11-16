9dc57156726a622b3949e3b9d227fc9bcd3c7c35
Muhamedall <112777103+Muhamedall@users.noreply.github.com>
2023-11-16 20:11:03 +0100

    Update Mylist.js

--- src/Form&redux/Mylist.js
+++ src/Form&redux/Mylist.js
@@ -1,11 +1,17 @@
 // Mylist.js
+import {  useSelector } from 'react-redux'
 export default function List({ listValue, onDelete, onEdit }) {
   const handleDelete = (index) => onDelete(index);
-  const handleEdit = (index) => onEdit(index);
-  
-  if (!Array.isArray(listValue) || listValue.length === 0) {
-    return <div>No items to display.</div>;
+  const handleEdit = (index,id) => onEdit(index,id);
+  const isloading = useSelector((state)=>state.isloading)
+  if (isloading===true) {
+    return <div>Chargement en cour</div>;
   }
+  else if (!Array.isArray(listValue) || listValue.length === 0) {
+    return <div> No items to display.</div>;
+  }
+  
+ 
 
   return (
     <>
@@ -14,7 +20,9 @@ export default function List({ listValue, onDelete, onEdit }) {
           <tr>
             <th>Email</th>
             <th>Name</th>
+           
             <th>Action</th>
+            
           </tr>
         </thead>
         <tbody>
@@ -22,9 +30,11 @@ export default function List({ listValue, onDelete, onEdit }) {
             <tr key={keys}>
               <td>{list.attributes.Email}</td>
               <td>{list.attributes.Name}</td>
+             
+             
               <td>
-                <button onClick={() => handleDelete(keys)}>Delete</button>
-                <button onClick={() => handleEdit(keys)}>Edit</button>
+                <button onClick={() => handleDelete(list.id)}>Delete</button>
+                <button onClick={() => handleEdit(keys,list.id)}>Edit</button>
               </td>
             </tr>
           ))}
